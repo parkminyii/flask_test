5dc59f53ab2a0741a210bce3ba11ab1afd532234
parkminyii <pemjia@gmail.com>
2023-03-24 15:22:10 +0900

    feat: 로그인 함수에 아이디, 비범을 넣어서 연동되게 구현

--- basic/d4.py
+++ basic/d4.py
@@ -3,7 +3,7 @@
 '''
 import pymysql as my
 
-def select_login():
+def select_login(uid, upw):
     connection = None
     try:
         connection = my.connect(host             = 'localhost',           
@@ -26,7 +26,7 @@ def select_login():
                     upw=%s;
             '''
             # execute() 함수의 2번 인자가 파라미터 전달하는 자리, 튜플로 표현
-            cursor.execute( sql, ('guest','1234') )
+            cursor.execute( sql, (uid, upw) )
             row = cursor.fetchone()
             print( row['name']) 
             pass
@@ -41,4 +41,7 @@ def select_login():
 if __name__ == '__main__':
     # d4 개발자의 테스트 코드
     # f5 개발자가 사용할 때는 작동 안함
-    select_login()
+    select_login('guest', '1234')
+#else: # d4가 아니라 다른 파일에서 돌릴 시 작동함 
+#    print('hihihih')
+#    print(__name__)
