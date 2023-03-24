32f9dfbec2aff3a8b85a92fc4babae969e13b3bc
parkminyii <pemjia@gmail.com>
2023-03-24 15:40:46 +0900

    feat: 로그인 실패 처리 기능 추가

--- basic/f5.py
+++ basic/f5.py
@@ -42,13 +42,16 @@ def login():
         upw = request.form.get('upw') # 암호는 차후에 암호화 해야 한다 (관리자도 볼 수 없다. 해싱)
         print( uid, upw )
         # 2. 회원 여부 쿼리
-        select_login(uid, upw) 
-        # 3. 회원이면
+        result = select_login(uid, upw)
+        if result: # 3. 회원이면
             # 3-1. 세션 생성, 기타 필요한 조치 수행
             # 3-2. 서비스 메인 화면으로 이동
-        # 4. 회원 아니면
+            pass
+        else:      # 4. 회원 아니면
             # 4-1. 적당한 메세지 후, 다시 로그인 유도 
-        return redirect('https://www.naver.com') # 요청을 다른 URL로 포워딩한다 
+            return render_template('error.html')
+            pass
+        #return redirect('https://www.naver.com') # 요청을 다른 URL로 포워딩한다 
 
 if __name__ == "__main__":
     app.run(debug=True)
